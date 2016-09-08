ab13ca4ba0765e315f96bb796726208c2bf802e4
Maksym Radiuk <Maksym_Radiuk@epam.com>
2016-09-08 15:52:54 +0300

    Added modules

---
+++ repositories/src/test/java/com/repo/FeedRepoTest.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FeedRepoTest {
+    @Test
+    public void getFeeds() throws Exception {
+
+    }
+
+}
---
+++ services/src/test/java/com/service/FeedServiceTest.java
@@ -0,0 +1,14 @@
+package com.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FeedServiceTest {
+
+    @Test
+    public void getAllFeeds() throws Exception {
+
+    }
+
+}
